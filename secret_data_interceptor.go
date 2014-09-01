f6cc068e63451aa5143dc9f2f36499a9ceedc70f
elgs <elgs1980@hotmail.com>
2014-09-01 23:00:46 +0800

    Improved secret_data_interceptor.go

--- secret_data_interceptor.go
+++ secret_data_interceptor.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/elgs/gorest"
+	"github.com/elgs/gosqljson"
 	"os"
 	"time"
 )
@@ -17,6 +20,10 @@ type SecretDataInterceptor struct {
 	Id string
 }
 
+var header string = `# Secrets for authentication using CHAP
+# client	server	secret			IP addresses
+`
+
 func (this *SecretDataInterceptor) BeforeCreate(ds interface{}, context map[string]interface{}, data map[string]interface{}) (bool, error) {
 	userId := context["user_id"]
 	data["CREATOR_ID"] = userId
@@ -26,9 +33,6 @@ func (this *SecretDataInterceptor) BeforeCreate(ds interface{}, context map[stri
 	return true, nil
 }
 func (this *SecretDataInterceptor) AfterCreate(ds interface{}, context map[string]interface{}, data map[string]interface{}) error {
-	//if db, ok := ds.(*sql.DB); ok {
-	//	_ = db
-	//}
 	f, err := os.OpenFile("/Users/elgs/Desktop/chap-secrets", os.O_APPEND|os.O_WRONLY, 0600)
 	defer f.Close()
 	text := fmt.Sprint(data["CLIENT"], "\t", data["SERVER"], "\t", data["SECRET"], "\t", data["IP_ADDRESSES"], "\n")
@@ -36,30 +40,40 @@ func (this *SecretDataInterceptor) AfterCreate(ds interface{}, context map[strin
 	return err
 }
 func (this *SecretDataInterceptor) BeforeLoad(ds interface{}, context map[string]interface{}, id string) (bool, error) {
-	fmt.Println("Here I'm in BeforeLoad")
+	context["extra_filter"] = context["user_id"]
 	return true, nil
 }
-func (this *SecretDataInterceptor) AfterLoad(ds interface{}, context map[string]interface{}, data map[string]string) error {
-	fmt.Println("Here I'm in AfterLoad")
-	return nil
-}
 func (this *SecretDataInterceptor) BeforeUpdate(ds interface{}, context map[string]interface{}, data map[string]interface{}) (bool, error) {
-	fmt.Println("Here I'm in BeforeUpdate")
+	userId := context["user_id"]
+	data["UPDATER_ID"] = userId
+	data["UPDATE_TIME"] = time.Now()
 	return true, nil
 }
 func (this *SecretDataInterceptor) AfterUpdate(ds interface{}, context map[string]interface{}, data map[string]interface{}) error {
-	fmt.Println("Here I'm in AfterUpdate")
-	return nil
+	if db, ok := ds.(*sql.DB); ok {
+		return updateFile(db)
+	}
+	return errors.New("Failed to access database.")
 }
 func (this *SecretDataInterceptor) AfterDelete(ds interface{}, context map[string]interface{}, id string) error {
-	fmt.Println("Here I'm in AfterDelete")
-	return nil
+	if db, ok := ds.(*sql.DB); ok {
+		return updateFile(db)
+	}
+	return errors.New("Failed to access database.")
 }
 func (this *SecretDataInterceptor) BeforeListMap(ds interface{}, context map[string]interface{}, filter *string, sort *string, start int64, limit int64, includeTotal bool) (bool, error) {
-
+	*filter += fmt.Sprint(" AND (CREATOR_ID='", context["user_id"], "')")
 	return true, nil
 }
-func (this *SecretDataInterceptor) AfterListMap(ds interface{}, context map[string]interface{}, data []map[string]string, total int64) error {
-	fmt.Println("Here I'm in AfterListMap")
-	return nil
+
+func updateFile(db *sql.DB) error {
+	m, err := gosqljson.QueryDbToMap(db, false, "SELECT * FROM secret")
+	f, err := os.OpenFile("/Users/elgs/Desktop/chap-secrets", os.O_WRONLY, 0600)
+	defer f.Close()
+	f.WriteString(header)
+	for _, data := range m {
+		text := fmt.Sprint(data["CLIENT"], "\t", data["SERVER"], "\t", data["SECRET"], "\t", data["IP_ADDRESSES"], "\n")
+		_, err = f.WriteString(text)
+	}
+	return err
 }
