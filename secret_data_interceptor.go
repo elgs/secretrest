276b2843689c6dfd0371878e7462506b4a8ea3aa
Elgs Chen <elgs1980@hotmail.com>
2014-09-06 00:44:51 +0800

    Bug fixes.

--- secret_data_interceptor.go
+++ secret_data_interceptor.go
@@ -47,7 +47,7 @@ func (this *SecretDataInterceptor) AfterCreate(ds interface{}, context map[strin
 	return err
 }
 func (this *SecretDataInterceptor) BeforeLoad(ds interface{}, context map[string]interface{}, id string) (bool, error) {
-	context["extra_filter"] = fmt.Sprint("AND USER_ID='", context["user_id"], "'")
+	context["extra_filter"] = fmt.Sprint("AND CREATOR_ID='", context["user_id"], "'")
 	return true, nil
 }
 func (this *SecretDataInterceptor) AfterLoad(ds interface{}, context map[string]interface{}, data map[string]string) error {
