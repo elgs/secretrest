edbb789fb0bb65a60ab29aa72a20ef396afa05ab
elgs <elgs1980@hotmail.com>
2015-01-20 09:36:39 -0800

    Update.

--- handlers.go
+++ handlers.go
@@ -30,25 +30,6 @@ func init() {
 			}
 		})
 
-	gorest.RegisterHandler("/last",
-		func(dbo gorest.DataOperator, gr *gorest.Gorest) func(w http.ResponseWriter, r *http.Request) {
-			return func(w http.ResponseWriter, r *http.Request) {
-				if r.FormValue("key") != gr.SessionKey {
-					fmt.Fprintln(w, "Attack!!!")
-					return
-				}
-
-				command := fmt.Sprint("last -F | grep ppp")
-				output, err := exec.Command("bash", "-c", command).CombinedOutput()
-				if err != nil {
-					fmt.Println("Failed to execute:", err, command)
-					fmt.Println(string(output))
-				} else {
-					fmt.Fprint(w, string(output))
-				}
-			}
-		})
-
 	gorest.RegisterHandler("/lastasof",
 		func(dbo gorest.DataOperator, gr *gorest.Gorest) func(w http.ResponseWriter, r *http.Request) {
 			return func(w http.ResponseWriter, r *http.Request) {
@@ -62,6 +43,29 @@ func init() {
 					asof = "19010101000000"
 				}
 				command := fmt.Sprint("lastasof -F -t " + asof + " | grep ppp")
+				output, err := exec.Command("bash", "-c", command).CombinedOutput()
+				if err != nil {
+					fmt.Println("Failed to execute:", err, command)
+					fmt.Println(string(output))
+				} else {
+					fmt.Fprint(w, string(output))
+				}
+			}
+		})
+
+	gorest.RegisterHandler("/ppp_lastasof",
+		func(dbo gorest.DataOperator, gr *gorest.Gorest) func(w http.ResponseWriter, r *http.Request) {
+			return func(w http.ResponseWriter, r *http.Request) {
+				if r.FormValue("key") != gr.SessionKey {
+					fmt.Fprintln(w, "Attack!!!")
+					return
+				}
+
+				asof := r.FormValue("asof")
+				if asof == "" {
+					asof = "19010101000000"
+				}
+				command := fmt.Sprint(`join -1 2 -2 1 <(last | grep 'ppp.*still' | sort -k2) <(cat /proc/net/dev | grep ppp | awk '{gsub(":","",$1); print $1, $2, $10}' | sort) ; lastasof -F -t ` + asof + " | grep ppp")
 				output, err := exec.Command("bash", "-c", command).CombinedOutput()
 				if err != nil {
 					fmt.Println("Failed to execute:", err, command)
